51b94bde0aa3611c3fdb617c49f9a13d8aa7fe47
Martina Dolezalova <123823082+xmardol40@users.noreply.github.com>
2024-12-03 22:35:59 +0100

    Update fourth.py

--- fourth.py
+++ fourth.py
@@ -1,3 +1,23 @@
+def moznosti_tahu(pozice, pohyby):
+    mozne_tahy = []
+    for i in range(0,len(pohyby)):
+            prvek = pohyby[i]
+            tup = []
+            for cislo in range(0,2):
+                prvni_cislo = pozice[cislo] + prvek[cislo]
+                tup.append(prvni_cislo)
+                if len(tup) == 2:
+                    tup = tuple(tup)
+                    mozne_tahy.append(tup)
+    return mozne_tahy
+
+def neco_stoji_v_ceste(mozne_tahy, obsazene_pozice):
+    for i in obsazene_pozice:
+        if i in mozne_tahy:
+            return True
+
+
+
 def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
     """
     Ověří, zda se figurka může přesunout na danou pozici.
@@ -10,6 +30,15 @@ def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
     """
     # Implementace pravidel pohybu pro různé figury zde.
 
+    pohyby_1 = [(1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0)]
+    pohyby_2 = [(-1,0), (-2,0), (-3,0), (-4,0), (-5,0), (-6,0), (-7,0)]
+    pohyby_3 = [(0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7)]
+    pohyby_4 = [(0,-1), (0,-2), (0,-3), (0,-4), (0,-5), (0,-6), (0,-7)]
+    pohyby_5 = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7)]
+    pohyby_6 = [(-1,1), (-2,2), (-3,3), (-4,4), (-5,5), (-6,6), (-7,7)]
+    pohyby_7 = [(1,-1), (2,-2), (3,-3), (4,-4), (5,-5), (6,-6), (7,-7)]
+    pohyby_8 = [(-1,-1), (-2,-2), (-3,-3), (-4,-4), (-5,-5), (-6,-6), (-7,-7)]
+
     if cilova_pozice[0] > 8:
         return False
     elif cilova_pozice[1] > 8:
@@ -18,75 +47,161 @@ def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
     if cilova_pozice in obsazene_pozice:
         return False
     
-    if figurka == pesec:
+    if figurka.get("typ") == "pěšec":
         pozice = figurka.get("pozice")
         pohyby = [(1,0), (2,0)]
+        mozne_tahy = moznosti_tahu(pozice, pohyby)
 
-        mozne_tahy = []
-
-        for i in range(0,len(pohyby)):
-            prvek = pohyby[i]
-            tup = []
-            for cislo in range(0,2):
-                prvni_cislo = pozice[cislo] + prvek[cislo]
-                tup.append(prvni_cislo)
-                if len(tup) == 2:
-                    tup = tuple(tup)
-                    mozne_tahy.append(tup)
-                #mozne_tahy = {(3,2),(4,2)}
-        if cilova_pozice in mozne_tahy:
+        zabrana = neco_stoji_v_ceste(mozne_tahy, obsazene_pozice)
+        if zabrana ==True:
+            return False            
+        elif cilova_pozice in mozne_tahy:
             return True
+        else:
+            return False
     
-    elif figurka == jezdec:
+    elif figurka.get("typ") == "jezdec":
         pozice = figurka.get("pozice")
         pohyby = [(-1,2), (1,2),(-2,1),(2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)]
-
-        mozne_tahy = []
-
-        for i in range(0,len(pohyby)):
-            prvek = pohyby[i]
-            tup = []
-            for cislo in range(0,2):
-                prvni_cislo = pozice[cislo] + prvek[cislo]
-                tup.append(prvni_cislo)
-                if len(tup) == 2:
-                    tup = tuple(tup)
-                    mozne_tahy.append(tup)
-        #mozne_tahy = {(1,2),(1,4),(2,1),(2,5),(4,1),(4,5),(5,2),(5,4)}
+        mozne_tahy = moznosti_tahu(pozice, pohyby)
         if cilova_pozice in mozne_tahy:
             return True
-    elif figurka == dama:
-        mozne_tahy = {(6,1),(7,2),(7,3),(7,4),(6,5),(5,6),(4,7),(3,8),(8,4),(8,5),(8,6),(8,7)}
-        if cilova_pozice in mozne_tahy:
-            return True
-    elif figurka == vez:
-        mozne_tahy = {(1,8),(2,8),(3,8),(4,8),(5,8),(6,8),(7,8),(8,7),(8,6),(8,5),(8,4)}
-        if cilova_pozice in mozne_tahy:
-            return True
-    elif figurka == kral:
+        else:
+            return False
+    
+    elif figurka.get("typ") == "král":
         pozice = figurka.get("pozice")
         pohyby = [(-1,1), (0,1),(1,1),(-1,0),(1,0),(-1,-1),(0,-1),(1,-1)]
-
-        mozne_tahy = []
-
-        for i in range(0,len(pohyby)):
-            prvek = pohyby[i]
-            tup = []
-            for cislo in range(0,2):
-                prvni_cislo = pozice[cislo] + prvek[cislo]
-                tup.append(prvni_cislo)
-                if len(tup) == 2:
-                    tup = tuple(tup)
-                    mozne_tahy.append(tup)
-                #mozne_tahy = {(1,5),(2,5),(2,4),(2,3),(1,3)}
+        mozne_tahy = moznosti_tahu(pozice, pohyby)
         if cilova_pozice in mozne_tahy:
             return True
-    elif figurka == strelec:
-        mozne_tahy = {(4,1), (5,2),(7,2),(8,1),(7,4),(8,5)}
-        if cilova_pozice in mozne_tahy:
-            return True
+        else:
+            return False
+        
+    elif figurka.get("typ") == "věž":
+        pozice = figurka.get("pozice")
+        mozne_tahy_1 = moznosti_tahu(pozice, pohyby_1)
+        mozne_tahy_2 = moznosti_tahu(pozice, pohyby_2)
+        mozne_tahy_3 = moznosti_tahu(pozice, pohyby_3)
+        mozne_tahy_4 = moznosti_tahu(pozice, pohyby_4)
+        zabrana_1 = neco_stoji_v_ceste(mozne_tahy_1, obsazene_pozice)
+        zabrana_2 = neco_stoji_v_ceste(mozne_tahy_2, obsazene_pozice)
+        zabrana_3 = neco_stoji_v_ceste(mozne_tahy_3, obsazene_pozice)
+        zabrana_4 = neco_stoji_v_ceste(mozne_tahy_4, obsazene_pozice)
+    
+        if cilova_pozice in mozne_tahy_1:
+            if zabrana_1 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_2:
+            if zabrana_2 == True:
+                return False
+            else:
+                return True    
+        elif cilova_pozice in mozne_tahy_3:
+            if zabrana_3 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_4:
+            if zabrana_4 == True:
+                return False
+            else:
+                return True
+            
+    elif figurka.get("typ") == "střelec":
+        pozice = figurka.get("pozice")
+        mozne_tahy_5 = moznosti_tahu(pozice, pohyby_5)
+        mozne_tahy_6 = moznosti_tahu(pozice, pohyby_6)
+        mozne_tahy_7 = moznosti_tahu(pozice, pohyby_7)
+        mozne_tahy_8 = moznosti_tahu(pozice, pohyby_8)
+        zabrana_5 = neco_stoji_v_ceste(mozne_tahy_5, obsazene_pozice)
+        zabrana_6 = neco_stoji_v_ceste(mozne_tahy_6, obsazene_pozice)
+        zabrana_7 = neco_stoji_v_ceste(mozne_tahy_7, obsazene_pozice)
+        zabrana_8 = neco_stoji_v_ceste(mozne_tahy_8, obsazene_pozice)
+        
+        if cilova_pozice in mozne_tahy_5:
+            if zabrana_5 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_6:
+            if zabrana_6 == True:
+                return False
+            else:
+                return True    
+        elif cilova_pozice in mozne_tahy_7:
+            if zabrana_7 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_8:
+            if zabrana_8 == True:
+                return False
+            else:
+                return True
 
-    return False
+    elif figurka.get("typ") == "dáma":
+        pozice = figurka.get("pozice")
+        mozne_tahy_1 = moznosti_tahu(pozice, pohyby_1)
+        mozne_tahy_2 = moznosti_tahu(pozice, pohyby_2)
+        mozne_tahy_3 = moznosti_tahu(pozice, pohyby_3)
+        mozne_tahy_4 = moznosti_tahu(pozice, pohyby_4)
+        mozne_tahy_5 = moznosti_tahu(pozice, pohyby_5)
+        mozne_tahy_6 = moznosti_tahu(pozice, pohyby_6)
+        mozne_tahy_7 = moznosti_tahu(pozice, pohyby_7)
+        mozne_tahy_8 = moznosti_tahu(pozice, pohyby_8)
+        zabrana_1 = neco_stoji_v_ceste(mozne_tahy_1, obsazene_pozice)
+        zabrana_2 = neco_stoji_v_ceste(mozne_tahy_2, obsazene_pozice)
+        zabrana_3 = neco_stoji_v_ceste(mozne_tahy_3, obsazene_pozice)
+        zabrana_4 = neco_stoji_v_ceste(mozne_tahy_4, obsazene_pozice)
+        zabrana_5 = neco_stoji_v_ceste(mozne_tahy_5, obsazene_pozice)
+        zabrana_6 = neco_stoji_v_ceste(mozne_tahy_6, obsazene_pozice)
+        zabrana_7 = neco_stoji_v_ceste(mozne_tahy_7, obsazene_pozice)
+        zabrana_8 = neco_stoji_v_ceste(mozne_tahy_8, obsazene_pozice)
+        
+        if cilova_pozice in mozne_tahy_1:
+            if zabrana_1 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_2:
+            if zabrana_2 == True:
+                return False
+            else:
+                return True    
+        elif cilova_pozice in mozne_tahy_3:
+            if zabrana_3 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_4:
+            if zabrana_4 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_5:
+            if zabrana_5 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_6:
+            if zabrana_6 == True:
+                return False
+            else:
+                return True    
+        elif cilova_pozice in mozne_tahy_7:
+            if zabrana_7 == True:
+                return False
+            else:
+                return True
+        elif cilova_pozice in mozne_tahy_8:
+            if zabrana_8 == True:
+                return False
+            else:
+                return True
+        
 
 
 if __name__ == "__main__":
