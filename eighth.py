4a542ab69421a418ba779f2e343e938883691f8d
Martina Dolezalova <123823082+xmardol40@users.noreply.github.com>
2024-12-12 12:34:25 +0100

    Update eighth.py

--- eighth.py
+++ eighth.py
@@ -8,11 +8,13 @@ def bin_to_dec(binarni_cislo):
     if binarni_cislo == "0":
         return 0
     elif bin_cislo_int < 0:
-      return f"Toto není binární číslo"
+        return f"Toto není binární číslo"
     else:
         for i in range(0, len(binarni_cislo)):
-          int(binarni_cislo[len(binarni_cislo)-1])*(2**i) += cislo
-      return cislo
+          index = len(binarni_cislo)-1-i
+          vypocet = int(binarni_cislo[index])*(2**i)
+          cislo += vypocet
+        return cislo
 
 
 
@@ -23,3 +25,4 @@ def test_bin_to_dec():
     assert bin_to_dec(101) == 5
     assert bin_to_dec("010101") == 21
     assert bin_to_dec(10000000) == 128
+
